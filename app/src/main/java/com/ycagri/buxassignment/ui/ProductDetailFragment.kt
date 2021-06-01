5b8136349fd0791b3826dc597b7f3f28afbf0f26
ycagri <akkayacagri@gmail.com>
2021-06-01 10:52:33 +0300

    ProductDetailFragment is implemented.

--- app/src/main/java/com/ycagri/buxassignment/ui/ProductDetailFragment.kt
+++ app/src/main/java/com/ycagri/buxassignment/ui/ProductDetailFragment.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import com.ycagri.buxassignment.R
+import com.ycagri.buxassignment.databinding.FragmentProductDetailBinding
 import com.ycagri.buxassignment.di.Injectable
+import com.ycagri.buxassignment.util.autoCleared
 import javax.inject.Inject
 
 /**
@@ -23,16 +22,30 @@ class ProductDetailFragment : Fragment(), Injectable {
 
     private val productViewModel: ProductViewModel by activityViewModels { viewModelFactory }
 
+    var binding by autoCleared<FragmentProductDetailBinding>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+        binding = FragmentProductDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initBinding()
+    }
 
+    private fun initBinding() {
+        binding.product = productViewModel.product
+        binding.closingPrice = productViewModel.closingPrice
+        binding.currentPrice = productViewModel.currentPrice
+        binding.dayRangeLow = productViewModel.dayRangeLow
+        binding.dayRangeHigh = productViewModel.dayRangeHigh
+        binding.yearRangeLow = productViewModel.yearRangeLow
+        binding.yearRangeHigh = productViewModel.yearRangeHigh
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 }
